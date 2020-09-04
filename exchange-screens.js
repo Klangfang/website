f3f29edea30c5cde72c20116eda26ee6a6bc251e
Franca Winter <franca@atelierdisko.de>
2020-09-04 17:55:39 +0200

    Implement screen exchange functionality

---
+++ exchange-screens.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  const content = document.querySelector("#scroll-content--desktop");
+  let screen = document.querySelector(".background__img");
+  const sectionCollect = document.querySelector(".section--collect");
+  const sectionCollaborate = document.querySelector(".section--collaborate");
+  const sectionEdit = document.querySelector(".section--edit");
+
+  content.addEventListener("scroll", function () {
+    // Offset of element from viewport top
+    const offset = 300;
+    let sectionCollectTop = sectionCollect.getBoundingClientRect().top + offset;
+    let sectionCollaborateTop = sectionCollaborate.getBoundingClientRect().top + offset;
+    let sectionEditTop = sectionEdit.getBoundingClientRect().top + offset;
+
+    screen.src = "./assets/device_1.png";
+
+    if (sectionCollectTop < window.innerHeight && sectionCollectTop > 0) {
+      screen.src = "./assets/device_2.png";
+    }
+    if (sectionCollaborateTop < window.innerHeight && sectionCollaborateTop > 0) {
+      screen.src = "./assets/device_4.png";
+    }
+    if (sectionEditTop < window.innerHeight && sectionEditTop > 0) {
+      screen.src = "./assets/device_1.png";
+    }
+  }, false);
+});
