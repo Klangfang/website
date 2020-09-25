adcc42cc14c16fa070ccff5587db4fd09f3d42be
Franca Winter <franca@atelierdisko.de>
2020-09-25 15:42:50 +0200

    Exchange mobile screens

--- exchange-screens.js
+++ exchange-screens.js
@@ -1,9 +1,37 @@
 document.addEventListener("DOMContentLoaded", function (event) {
   const content = document.querySelector("#scroll-content--desktop");
+  const contentMobile = document.querySelector(".content--mobile");
   let screen = document.querySelector(".background__img");
+  let screenMobile = document.querySelector(".section-phone-content__image");
   const sectionCollect = document.querySelector(".section--collect");
+  const sectionCollectMobile = document.querySelector(".section--collect-mobile");
   const sectionCollaborate = document.querySelector(".section--collaborate");
+  const sectionCollaborateMobile = document.querySelector(".section--collaborate-mobile");
   const sectionEdit = document.querySelector(".section--edit");
+  const sectionEditMobile = document.querySelector(".section--edit-mobile");
+
+  contentMobile.addEventListener("scroll", function () {
+    // Offset of element from viewport top
+    const offset = 100;
+
+    let sectionCollectTopMobile = sectionCollectMobile.getBoundingClientRect().top + offset;
+    let sectionCollaborateTopMobile = sectionCollaborateMobile.getBoundingClientRect().top + offset;
+    let sectionEditTopMobile = sectionEditMobile.getBoundingClientRect().top + offset;
+
+    screenMobile.src = "./assets/device_1.png";
+
+    if (sectionCollectTopMobile < window.innerHeight && sectionCollectTopMobile > 0) {
+      screenMobile.src = "./assets/device_2.png";
+    }
+
+    if (sectionCollaborateTopMobile < window.innerHeight && sectionCollaborateTopMobile > 0) {
+      screenMobile.src = "./assets/device_3.png";
+    }
+
+    if (sectionEditTopMobile < window.innerHeight && sectionEditTopMobile > 0) {
+      screenMobile.src = "./assets/device_4.png";
+    }
+  }, false);
 
   content.addEventListener("scroll", function () {
     // Offset of element from viewport top
